e15587cf3d3206c38c53c4462d39bf4b5e9c0b6d
Alvin James Bellero <ajames.bellero@gmail.com>
2016-02-23 19:19:39 +0800

    Added inventory route file handle

---
+++ routes/api/inventory.js
@@ -0,0 +1,42 @@
+(function() {
+  var Inventory = require('../../models/inventory')
+
+  module.exports = function(app) {
+
+    getItems = function(req, res) {
+      var retVal = {};
+      Inventory.getAllItems(function(err, items) {
+        if(err) return res.status(500).send(err);
+        if(req.query.datatable) retVal = {data: items}; else retVal = items;
+        res.status(200).json(retVal);
+      });
+    }
+
+    createItem = function(req, res) {
+      Inventory.saveItem(req.body, function(err, createdItem) {
+        if(err) return res.status(500).send(err);
+        res.status(201).json(createdItem);
+      });
+    }
+
+    updateItem = function(req, res) {
+      Inventory.updateItem(req.params.itemID, req.body, function(err, updatedObj) {
+        if(err) return res.status(500).send(err);
+        res.status(200).json(updatedObj)
+      });
+    }
+
+    removeItem = function(req, res) {
+      Inventory.removeItem(req.params.itemID, function(err, deletedObj) {
+        if(err) return res.status(500).send(err);
+        res.status(200).json(deletedObj)
+      });
+    }
+
+    app.post('/api/inventory', createItem);
+    app.put('/api/inventory/:itemID', updateItem);
+    app.delete('/api/inventory/:itemID', removeItem);
+    app.get('/api/inventory', getItems);
+
+  }
+}).call(this)
