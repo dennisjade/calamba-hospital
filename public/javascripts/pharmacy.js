02df974c4ee049b51bde0fd782c40b53a5d4a811
Alvin James Bellero <ajames.bellero@gmail.com>
2016-02-24 23:15:25 +0800

    Created js file for pharmacy scripts

---
+++ public/javascripts/pharmacy.js
@@ -0,0 +1,132 @@
+(function() {
+
+  populateFields = function(obj) {
+    var data = JSON.parse(obj);
+    $("#overlay-edit-medicine-dialog #medicineID").val(data._id);
+    $("#overlay-edit-medicine-dialog #medicineName").val(data.medicineName);
+    $("#overlay-edit-medicine-dialog #medicineQuantity").val(data.medicineQuantity);
+    $("#overlay-edit-medicine-dialog #medicineDesc").val(data.medicineDesc);
+  }
+
+  validateFields = function($frm) {
+    var medicine_name = $frm.find('#medicineName').val();
+    if(!medicine_name) return false;
+    return true;
+  }
+
+  clearForm = function(_id) {
+    $(_id).find("#medicineName, #medicineDesc").val('');
+    $(_id).find("#medicineQuantity").val(1);
+  }
+
+  var medicineItems = $('#medicineItems').DataTable({
+    "ajax": "/api/medicines/?datatable=true",
+    "columns": [
+      { "data": "_id", "visible": false },
+      { "data": "updateDate" },
+      { "data": "medicineName" },
+      { "data": "medicineQuantity" },
+      { "data": "medicineDesc" },
+      { "data": "Actions" }
+    ],
+
+    "columnDefs": [
+      {
+        "targets": 1,
+        "render": function(d, t, r) {
+          return "<span title='"+moment(r.updateDate).format('LT')+"'>"+moment(r.updateDate).format('ll')+  "</span>";
+        }
+      },
+      { "targets": 2, "width": "30%" },
+      { "targets": 3, "className": "dt-center" },
+      {
+        "targets": 4,
+        "render": function(d, t, r) {
+          return "<div title='"+r.medicineDesc+"' class='truncate'>"+r.medicineDesc+"</div>"
+        }
+      },
+      {
+        "targets": 5,
+        "className": "dt-center",
+        "render": function(d, t, r) {
+          var markup = "<a class='label label-primary editMedicine' refObj='"+JSON.stringify(r)+"' ref='"+r._id+"' href='#'>EDIT</a>&nbsp;" +
+                       "<a class='label label-danger deleteMedicine' ref='"+r._id+"' href='#'>DELETE</a>"
+          return markup;
+        }
+      }
+    ],
+
+    "fnDrawCallback": function(settings) {
+
+      // Edit medicine modal
+      $('#medicineItems tr .editMedicine').unbind('click').click( function(e) {
+        e.preventDefault();
+        editMedicineDialog.dialog("open");
+        populateFields( $(this).attr('refObj') );
+      });
+
+      // Remove medicine modal
+      $('#medicineItems tr .deleteMedicine').unbind('click').click( function(e) {
+        e.preventDefault();
+        var isDelete = confirm("Delete current medicine?");
+
+        if(isDelete) {
+          var _id = $(this).attr('ref');
+          execAjax('/api/medicines/' + _id, 'DELETE', {}).success( function(resp) {
+            medicineItems.ajax.reload();
+          });
+        }
+
+      });
+
+    }
+  });
+
+  // Trigger create medicine dialog
+  $('#addMedicine').click(function() {
+    addMedicineDialog.dialog('open');
+    clearForm("#overlay-add-medicine-dialog form");
+  });
+
+  // Edit medicine UI dialog
+  var editBtnActions = {
+    "Update": function() {
+      var frm = $("#overlay-edit-medicine-dialog form").serialize(),
+          _id = $("#overlay-edit-medicine-dialog #medicineID").val();
+      // Execute update request
+      execAjax('/api/medicines/' + _id, 'PUT', frm).success( function(resp) {
+        editMedicineDialog.dialog('close');
+        medicineItems.ajax.reload();
+      });
+    },
+    "Cancel": function() {
+      $(this).dialog("close");
+    }
+  }
+
+  // Add medicine UI dialog
+  var addBtnActions = {
+    "Create": function() {
+      var $frm = $("#overlay-add-medicine-dialog form");
+      var serialData = $("#overlay-add-medicine-dialog form").serialize();
+
+      if(validateFields($frm)) {
+        // Execute POST/ create request
+        execAjax('/api/medicines/', 'POST', serialData).success( function(resp) {
+          addMedicineDialog.dialog('close');
+          medicineItems.ajax.reload();
+        });
+      } else {
+        alert("Please complete required fields.");
+      }
+    },
+    "Cancel": function() {
+      $(this).dialog("close");
+    }
+  }
+
+  // Initialize dialogs
+  var editMedicineDialog = initDialog("#overlay-edit-medicine-dialog", editBtnActions);
+  var addMedicineDialog = initDialog("#overlay-add-medicine-dialog", addBtnActions);
+
+}).call(this);
